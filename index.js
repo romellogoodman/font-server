34673da59eeeaeddb86e4d0f89aa7822ceef691a
Romello Goodman <romellogood@gmail.com>
2023-02-24 19:09:00 -0500

    Remove router

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const router = require("./router");
 
 const port = process.env.PORT || 3000;
 
 const server = express();
 
-server.use(router);
+server.use(express.static("public"));
+
+server.get("*", (req, res) => {
+  res.send("ok");
+});
 
 server.listen(port, () =>
   console.log(`Server running on ${port}, http://localhost:${port}`)
