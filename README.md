54205be7e01700a47c7852ff5d36bcac3d7b3c9e
Romello Goodman <romellogood@gmail.com>
2023-03-01 01:53:31 -0500

    Basic copy in the index and README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Font Server
 
-More tk
+[fonts.romellogoodman.com](https://fonts.romellogoodman.com/)
+
+This is an open source font server that hosts open source fonts. At the moment it is focused on hosting variable fonts.
+
+### Author
+
+[Romello Goodman](https://www.romellogoodman.com/)
--- index.11ty.js
+++ index.11ty.js
@@ -2,11 +2,15 @@ const renderHTML = () => {
   return `
     <html>
       <head>
-        <title>Font Server</title>
+        <title>Font Server | Romello Goodman</title>
       </head>
       <body>
         <main>
-          <p>Font Server.</p>
+          <h1>Font Server</h1>
+          <p>This is an open source font server that hosts open source fonts. At the moment it is focused on hosting variable fonts.</p>
+          <a href="https://github.com/romellogoodman/font-server">
+            <p>Codebase Repo</p>
+          </a>
         </main>
       </body>
     </html>
